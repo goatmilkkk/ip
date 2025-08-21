902dda861548aea656c56c065915da80563c30a5
goatmilkkk <88575461+goatmilkkk@users.noreply.github.com>
2025-08-22 01:44:29 +0800

    Level 1. Echo

--- src/main/java/Jay.java
+++ src/main/java/Jay.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Jay {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Jay");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
     }
 }
