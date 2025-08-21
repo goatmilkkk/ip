2f94544e993e4d637dd71be376889b8f1b903ce4
goatmilkkk <88575461+goatmilkkk@users.noreply.github.com>
2025-08-22 01:56:57 +0800

    Level 2. Add, List

--- src/main/java/Jay.java
+++ src/main/java/Jay.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class Jay {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Jay");
-        System.out.println("What can I do for you?");
-
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int count = 0;
+
+        System.out.println("Hello! I'm Jay");
+        System.out.println("What can I do for you?\n");
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("");
+            } else {
+                tasks[count++] = input;
+                System.out.println("added: " + input + "\n");
             }
-            System.out.println(input);
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
