626b74eabe0f6ee040d79d7903cd28ced1bd55f8
goatmilkkk <88575461+goatmilkkk@users.noreply.github.com>
2025-08-22 02:22:37 +0800

    Level 3. Mark as Done

--- src/main/java/Jay.java
+++ src/main/java/Jay.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Jay {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
 
         System.out.println("Hello! I'm Jay");
@@ -15,14 +15,25 @@ public class Jay {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-                System.out.println("");
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.replace("mark", "").trim());
+                System.out.println("Nice! I've marked this task as done:");
+                tasks[taskNumber - 1].markAsDone();
+                System.out.println(tasks[taskNumber - 1]);
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.replace("unmark", "").trim());
+                System.out.println("OK, I've marked this task as not done yet:");
+                tasks[taskNumber - 1].unmarkAsDone();
+                System.out.println(tasks[taskNumber - 1]);
             } else {
-                tasks[count++] = input;
-                System.out.println("added: " + input + "\n");
+                tasks[count++] = new Task(input);
+                System.out.println("added: " + input);
             }
+            System.out.println();
         }
     }
 }
